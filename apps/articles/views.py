73933dafee776f239cb0aa10ad3944eb17c6661b
Андрей Спиридонов <and1997310366@mail.ru>
2019-09-19 00:04:11 +0700

    Проверочка

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 # Отображение url articles и articles/test/
 def index(request):
 	return HttpResponse("Привет мир!")
-	
+
 def test(request):
 	return HttpResponse("Это тестовая страница")
+	# Тестовое изменение 
