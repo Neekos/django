4c6fdad0f111ea7b079636b92ffae46688b30b3f
Андрей Спиридонов <and1997310366@mail.ru>
2019-09-19 00:26:36 +0700

    Описание создание моделей

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-# Создаю первую модель, они создаются классами
- class Article
+# Создаю первую модель, они создаются классами, принято писать в единственном числе
+#Модель Заголовков
+class Article(models.Model):
+	pass
+#Модель комментариев
+class Comment(models.Model):
+	pass
