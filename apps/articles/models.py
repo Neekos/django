58a031211c1d282b43f2112e8f63a32b99674086
Андрей Спиридонов <and1997310366@mail.ru>
2019-09-19 00:34:04 +0700

    Определяю поля Статить и их типы данных

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 # Создаю первую модель, они создаются классами, принято писать в единственном числе
-#Модель Заголовков
+#Модель Статьи
 class Article(models.Model):
-	pass
-#Модель комментариев
+	#Создаю поля Статьии заголовок. CharField == Varchar mysql
+	article_title = models.CharField('Название статьи',max_length = 200)
+	#Тип данных Textfield для набора текстов более 255 символов
+	article_text = models.TextField('Текс Статьи')
+	#Тип данных DateTimeField дата время
+	pub_date = models.Date.DateTimeField('Дата Публикации')
+#Модель комментария
 class Comment(models.Model):
 	pass
